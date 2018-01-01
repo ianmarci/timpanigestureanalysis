f0adb10ae77bcf49903f00e98118fc7835495b07
ianmarci <ianmarci@aol.com>
2018-01-01 16:46:52 -0500

    Added error checking and output location of divide_into_sets. changed names of plotting functions, added confusion matrix to run_experiment.

--- divide_into_sets.py
+++ divide_into_sets.py
@@ -4,41 +4,68 @@ import shutil
 import sys
 import os
 
+################################################################################
+# Function which randomly divides files in the selected folder into 4 equal    #
+# sets. User chooses which data folder to use as well as the hand to study.    #
+#                                                                              #
+# Recommended usage: python divide_into_sets.py 'Data/StrokePositionData' R    #
+#                                                                              #
+# Data would be 200 position points of the right mallet.                       #
+# Data sets will be placed in 'Data/NetworkInput' for future classification.   #
+################################################################################
+
+if len(sys.argv) != 3:
+    print('Usage error. Incorrect number of arguments.')
+    print('Usage: python divide_into_sets.py path_to_data hand_choice')
+    sys.exit()
+
 data_path = sys.argv[1]
+hand_choice = sys.argv[2]
+
+if hand_choice != 'R' and hand_choice != 'L':
+    print('Usage error. hand_choice must be either R or L')
+    sys.exit()
+
+out_path = 'Data\\NetworkInput\\'
 
 # Find files in target folder
-files = [file for file in glob.glob(data_path + '/*.txt')]
+data_files = [file for file in glob.glob(data_path + '/*.txt')]
 
-# Fix incorrect backslashes
-# Parsed by glob to    path \\ file.txt
-data_files = []
-for filename in files:
-    parts = filename.split('\\')
-    outfile = parts[1]
-    data_files.append(outfile)
+if len(data_files) == 0:
+    print('Check data path. File not found.')
+    sys.exit()
+
+if hand_choice == 'R':
+    temp = [filename for filename in data_files if 'L' not in filename]
+else:
+    temp = [filename for filename in data_files if 'L' in filename]
+
+data_files = temp
 
 random.shuffle(data_files)
 
 sets = ['Set 1', 'Set 2', 'Set 3', 'Set 4']
-data_path = data_path + '/'
+data_path = data_path + '\\'
 
 # Create set folders
 for Set in sets:
-    os.makedirs(data_path + Set)
+    os.makedirs(out_path + Set)
 
 # Fill each folder
 n = 0
-for filename in data_files:
+for path in data_files:
+    parts = path.split('\\')
+    file_name = parts[2]
     if n % 4 == 0:
-        final_path = data_path + sets[0]
-        shutil.copy2(data_path + filename, final_path)
+        final_path = out_path + sets[0] + '\\' + file_name
+        shutil.copy2(path, final_path)
     if n % 4 == 1:
-        final_path = data_path + sets[1]
-        shutil.copy2(data_path + filename, final_path)
+        final_path = out_path + sets[1] + '\\' + file_name
+        shutil.copy2(path, final_path)
     if n % 4 == 2:
-        final_path = data_path + sets[2]
-        shutil.copy2(data_path + filename, final_path)
+        final_path = out_path + sets[2] + '\\' + file_name
+        shutil.copy2(path, final_path)
     if n % 4 == 3:
-        final_path = data_path + sets[3]
-        shutil.copy2(data_path + filename, final_path)
+        final_path = out_path + sets[3] + '\\' + file_name
+        shutil.copy2(path, final_path)
     n += 1
--- run_experiment.py
+++ run_experiment.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 import numpy as np
 
-from data_input_functions import choose_test_set, get_network_input
+from classifier_input_functions import choose_test_set, get_network_input
 
-trainpath = 'Data/NetworkTrain/'
-testpath = 'Data/NetworkTest/'
+train_path = 'Data/NetworkTrain/'
+test_path = 'Data/NetworkTest/'
 
 x_train = tf.placeholder('float', [None, 200])
 x_test = tf.placeholder('float', [200])
@@ -17,21 +17,26 @@ init = tf.global_variables_initializer()
 with tf.Session() as sess:
     for i in range(4):
         sess.run(init)
-        accuracy = 0
+
         choice = i + 1
         choose_test_set(str(choice))
 
-        train_data, train_labels = get_network_input(trainpath)
-        test_data, test_labels = get_network_input(testpath)
+        train_data, train_labels = get_network_input(train_path)
+        test_data, test_labels = get_network_input(test_path)
+
+        accuracy = 0
+        predictions = np.zeros((len(test_data)))
+        labels = np.zeros((len(test_data)))
 
         for i in range(len(test_data)):
-            knn_index = sess.run(pred, feed_dict={x_train: train_data,
-                                                  x_test: test_data[i, :]})
-            prediction = np.argmax(train_labels[knn_index])
-            correct_answer = np.argmax(test_labels[i])
-            print('Test', i, 'Prediction: ', prediction,
-                'True Class: ', correct_answer)
-            if prediction == correct_answer:
+            nn_index = sess.run(pred, feed_dict={x_train: train_data,
+                                                 x_test: test_data[i, :]})
+            predictions[i] = np.argmax(train_labels[nn_index])
+            labels[i] = np.argmax(test_labels[i])
+            print('Test', i, 'Prediction: ', predictions[i],
+                'True Class: ', labels[i])
+            if predictions[i] == labels[i]:
                 accuracy += 1./len(test_data)
-
+        confusion = tf.confusion_matrix(labels=labels, predictions=predictions)
+        print(confusion.eval())
         print('Test Accuracy:', accuracy)
